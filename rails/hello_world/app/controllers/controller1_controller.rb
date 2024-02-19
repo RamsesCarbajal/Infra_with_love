5aab3a245cdb2b5490a803b9fc868fa046e70358
Ramses Carbajal <ramses@bitso.com>
2024-02-19 10:26:50 -0600

    update rails code to show hello world in the front and show some variables from the controller

--- rails/hello_world/app/controllers/controller1_controller.rb
+++ rails/hello_world/app/controllers/controller1_controller.rb
@@ -1,4 +1,7 @@
 class Controller1Controller < ApplicationController
+  attr_accessor :controller_var, :env_var
   def index
+    @controller_var = "from Controller1Controller"
+    @env_var = ENV["CUSTOM_ENV_LOVE"]
   end
 end
